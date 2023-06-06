a81841dc0de97da61d0571a1e89c3f3d19dbdde2
CameronLong <57040372+CameronLong@users.noreply.github.com>
2023-06-05 21:49:51 -0400

    Setup topbar, packages, and routes
    I setup the topbar for the app, as well as all the routes and packages.

---
+++ src/scenes/global/Sidebar.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import { ProSidebar, Menu, MenuItem } from "react-pro-sidebar";
+
+
+const Sidebar = () => {
+    <div>
+        Sidebar
+    </div>
+}
+
+export default Sidebar;
