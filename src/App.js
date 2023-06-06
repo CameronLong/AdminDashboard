f0d9c427ed465ff0cb1efa1c28da42aefce6db2b
CameronLong <57040372+CameronLong@users.noreply.github.com>
2023-06-05 21:53:30 -0400

    General file changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './index.css'
 import Topbar from './scenes/global/Topbar';
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
-import { Routes } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Sidebar from './scenes/global/Sidebar';
 import Dashboard from './scenes/dashboard';
 // import Team from './scenes/team';
@@ -45,7 +45,7 @@ function App() {
         </div>
       </ThemeProvider>
     </ColorModeContext.Provider>
-  );
+  )
 }
 
 export default App;
