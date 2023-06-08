efeb128b1cd1b865e30750cf0111aef8d8f71f48
CameronLong <57040372+CameronLong@users.noreply.github.com>
2023-06-07 20:35:46 -0400

    Work on sidebar
    Worked on the sidebar and team components

--- src/scenes/global/AppSidebar.js
+++ src/scenes/global/AppSidebar.js
@@ -13,6 +13,7 @@ import HelpOutlinedIcon from '@mui/icons-material/HelpOutlined';
 import BarChartOutlinedIcon from '@mui/icons-material/BarChartOutlined';
 import PieChartOutlinedIcon from '@mui/icons-material/PieChartOutlined';
 import TimelineOutlinedIcon from '@mui/icons-material/TimelineOutlined';
+import LineChartOutlinedIcon from '@mui/icons-material/StackedLineChartOutlined';
 import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
 import MapOutlinedIcon from '@mui/icons-material/MapOutlined';
 
@@ -55,22 +56,90 @@ const AppSidebar = () => {
                     </MenuItem>
                     {!isCollapsed && (
                         <Box mb={'25px'}>
-                            <Box  display={'flex'} justifyContent={'center'} alignItems={'center'}>
-                                <img 
+                            <Box display={'flex'} justifyContent={'center'} alignItems={'center'}>
+                                <img
                                     alt="profile-user"
                                     width={'100px'}
                                     height={'100px'}
                                     src={'../../assets/user.png'}
-                                    style={{ cursor: 'pointer', borderRadius: '50%'}}
+                                    style={{ cursor: 'pointer', borderRadius: '50%' }}
                                 />
                             </Box>
 
                             <Box textAlign={'center'}>
-                                <Typography variant="h2" color={colors.grey[100]} fontWeight={'bold'} sx={{ m: '10px 0 0 0'}}>Cameron Long</Typography>
+                                <Typography variant="h2" color={colors.grey[100]} fontWeight={'bold'} sx={{ m: '10px 0 0 0' }}>Cameron Long</Typography>
                                 <Typography variant="h5" color={colors.greenAccent[500]}>Admin</Typography>
                             </Box>
                         </Box>
                     )}
+                    <Box paddingLeft={isCollapsed ? undefined : '10%'}>
+                        <MenuItem
+                            to="/"
+                            icon={<HomeOutlinedIcon />}
+                            selected={selected}
+                            setSelected={setSelected}
+                        >Dashboard</MenuItem>
+                        <MenuItem
+                            to="/team"
+                            icon={<PeopleOutlinedIcon />}
+                            selected={selected}
+                            setSelected={setSelected}
+                        >Team</MenuItem>
+                        <MenuItem
+                            to="/contacts"
+                            icon={<ContactsOutlinedIcon />}
+                            selected={selected}
+                            setSelected={setSelected}
+                        >Contacts Information</MenuItem>
+                        <MenuItem
+                            to="/invoices"
+                            icon={<ReceiptOutlinedIcon />}
+                            selected={selected}
+                            setSelected={setSelected}
+                        >Invoices Balances </MenuItem>
+                        <MenuItem
+                            to="/form"
+                            icon={<PersonOutlinedIcon />}
+                            selected={selected}
+                            setSelected={setSelected}
+                        >Profile Form </MenuItem>
+                        <MenuItem
+                            to="/calendar"
+                            icon={<CalendarTodayOutlinedIcon />}
+                            selected={selected}
+                            setSelected={setSelected}
+                        >Calendar </MenuItem>
+                        <MenuItem
+                            to="/faq"
+                            icon={<HelpOutlinedIcon />}
+                            selected={selected}
+                            setSelected={setSelected}
+                        >FAQ page</MenuItem>
+                        <MenuItem
+                            to="/bar"
+                            icon={<BarChartOutlinedIcon />}
+                            selected={selected}
+                            setSelected={setSelected}
+                        >Bar Chart </MenuItem>
+                        <MenuItem
+                            to="/pie"
+                            icon={<PieChartOutlinedIcon />}
+                            selected={selected}
+                            setSelected={setSelected}
+                        >Pie Chart </MenuItem>
+                        <MenuItem title="Line Chart"
+                            to="/line"
+                            icon={<LineChartOutlinedIcon />}
+                            selected={selected}
+                            setSelected={setSelected}
+                        >Line Chart </MenuItem>
+                        <MenuItem title="Geography Chart"
+                            to="/geography"
+                            icon={<MapOutlinedIcon />}
+                            selected={selected}
+                            setSelected={setSelected}
+                        >Geography Chart </MenuItem>
+                    </Box>
                 </Menu>
             </Sidebar>
 
