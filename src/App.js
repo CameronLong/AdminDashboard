c825cbadfc3093e86fc25c7297db1f4589bd9b0a
CameronLong <57040372+CameronLong@users.noreply.github.com>
2023-06-09 13:55:49 -0400

    Finish table pages
    Finished the pages that will be using tables.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { Routes, Route } from 'react-router-dom';
 import AppSidebar from './scenes/global/AppSidebar';
 import Dashboard from './scenes/dashboard';
 import Team from './scenes/team';
-// import Invoices from './scenes/invoices';
+import Invoices from './scenes/invoices';
 import Contacts from './scenes/contacts';
 // import Bar from './scenes/bar';
 // import Form from './scenes/form';
@@ -31,7 +31,7 @@ function App() {
             <Routes>
               <Route path="/" element={<Dashboard />}/>
               <Route path="/team" element={<Team />}/>
-              {/* <Route path="/invoices" element={<Invoices />}/> */}
+              <Route path="/invoices" element={<Invoices />}/>
               <Route path="/contacts" element={<Contacts />}/>
               {/* <Route path="/bar" element={<Bar />}/> */}
               {/* <Route path="/form" element={<Form />}/> */}
