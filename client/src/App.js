66904e01d28ce0e208e3b4e93d9c88f55e13348c
CameronLong <57040372+CameronLong@users.noreply.github.com>
2023-07-15 14:32:20 -0600

    Work on data grid
    Worked on the data grid component

--- client/src/App.js
+++ client/src/App.js
@@ -30,7 +30,7 @@ function App() {
             <Topbar />
             <Routes>
               <Route path="/" element={<Dashboard />}/>
-              <Route path="/team" element={<Team />}/>
+              {/* <Route path="/team" element={<Team />}/> */}
               {/* <Route path="/invoices" element={<Invoices />}/> */}
               <Route path="/contacts" element={<Contacts />}/>
               {/* <Route path="/bar" element={<Bar />}/> */}
